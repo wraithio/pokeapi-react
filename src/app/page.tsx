4883322412265c7cec79a3b6e594c75deeb49040
wraithio <aarmon5@gmail.com>
2025-03-27 22:41:41 -0700

    just need to format favorites on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { generateFavs, generatePokemonData } from "@/lib/services";
+import { deformatString, generateFavs, generatePokemonData } from "@/lib/services";
 import { useEffect, useState } from "react";
 import { PokemonData, IFavorites } from "../../interfaces/interfaces";
 import {
@@ -36,11 +36,11 @@ export default function Home() {
   };
 
   const addtoFavs = async () => {
-    saveToLocalStorageByName(data.name.toLowerCase());
+    saveToLocalStorageByName(deformatString(data.name));
     setFav(await generateFavs());
   };
   const removeFav = async (pokemon: string) => {
-    removeFromLocalStorage(pokemon.toLowerCase());
+    removeFromLocalStorage(deformatString(pokemon));
     setFav(await generateFavs());
   };
 
@@ -49,19 +49,21 @@ export default function Home() {
       setFav(await generateFavs());
     };
     callFavs();
+    displayData(String(Math.floor(Math.random() * 649) + 1))
   }, []);
 
   return (
-    <div className="font-[family-name:var(--font-bubblegum-sans)] min-h-screen flex justify-center flex-col mx-3">
-      <div className="flex flex-row justify-between">
+    <div className="font-[family-name:var(--font-bubblegum-sans)] min-h-screen flex sm:justify-center flex-col mx-3">
+      <div className="flex lg:flex-row flex-col-reverse justify-between">
         <div className="flex gap-3 place-items-center mb-2">
           <Input
             name="pokemon"
             onChange={(event) => setSearch(event.target.value)}
+            className="lg:w-fit w-48 sm:w-fit"
           />
           <Button
             type="submit"
-            className="cursor-pointer"
+            className="cursor-pointer sm:text-md text-sm"
             variant="outline"
             onClick={() => displayData(search)}
           >
@@ -69,7 +71,7 @@ export default function Home() {
           </Button>
           <Button
             type="submit"
-            className="text-md cursor-pointer"
+            className="text-md cursor-pointer sm:text-md text-sm"
             variant="outline"
             onClick={() =>
               displayData(String(Math.floor(Math.random() * 649) + 1))
@@ -84,37 +86,37 @@ export default function Home() {
             onClick={addtoFavs}
           />
         </div>
-        <div className="flex gap-2">
-          <h1 className="text-7xl">Pokedex</h1>
+        <div className="flex sm:gap-2 gap-1 sm:justify-normal justify-end sm:mt-0 mt-2">
+          <h1 className="sm:text-7xl text-sm">Pokedex</h1>
           <div className="flex place-items-end mb-2">
-            <h2 className="text-2xl">powered by PokeAPI</h2>
+            <h2 className="sm:text-2xl text-sm">powered by PokeAPI</h2>
           </div>
         </div>
       </div>
 
       <div className="grid grid-cols-[3fr_2fr] gap-3">
-        <div className="grid grid-cols-2 grid-rows-[repeat(3,auto)] gap-3">
-          <div className="col-start-1 border-2 border-black p-2">
-            <h2 className="text-4xl">{data.name}</h2>
-            <div className="flex gap-1">
+        <div className="grid grid-cols-2 sm:grid-rows-[repeat(3,auto)] grid-rows-[150px_270px_1fr] sm:gap-3">
+          <div className="col-start-1 sm:col-end-2 col-end-3 sm:border-2 border-black sm:p-2">
+            <h2 className="sm:text-4xl text-5xl sm:my-0 my-2">{data.name}</h2>
+            <div className="flex gap-1 sm:text-xl">
               <div className="flex flex-col text-right">
-                <h3 className="text-xl">no.</h3>
-                <h3 className="text-xl">type:</h3>
-                <h3 className="text-xl">location:</h3>
+                <h3>no.</h3>
+                <h3>type:</h3>
+                <h3>location:</h3>
               </div>
               <div className="flex flex-col">
-                <h3 className="text-xl">{data.number}</h3>
-                <h3 className="text-xl">{data.type.toString()}</h3>
-                <h3 className="text-xl"> {data.location}</h3>
+                <h3>{data.number}</h3>
+                <h3>{data.type.toString()}</h3>
+                <h3> {data.location}</h3>
               </div>
             </div>
           </div>
-          <div className="relative col-start-2 row-start-1 row-end-3 border-2 border-black p-2 flex place-items-center">
+          <div className="relative sm:col-start-2 col-start-1 sm:col-end-2 col-end-3 sm:row-start-1 row-start-2 row-end-3 sm:border-2 border-black sm:p-2 flex place-items-center">
             <h1
               className={
                 data.number <= 1
                   ? "hidden"
-                  : "text-xl hover:text-slate-500 cursor-pointer"
+                  : "lg:text-xl text-sm hover:text-slate-500 cursor-pointer"
               }
               onClick={() => displayData(String(data.number - 1))}
             >
@@ -128,14 +130,14 @@ export default function Home() {
               <img
                 src={shiny ? data.shiny : data.picture}
                 alt={data.name}
-                className="w-full aspect-square p-2"
+                className="w-full aspect-square sm:p-2"
               />
             </a>
             <h1
               className={
                 data.number >= 649
                   ? "hidden"
-                  : "text-xl hover:text-slate-500 cursor-pointer"
+                  : "lg:text-xl text-sm hover:text-slate-500 cursor-pointer"
               }
               onClick={() => displayData(String(data.number + 1))}
             >
@@ -148,73 +150,80 @@ export default function Home() {
               onClick={() => setShiny(!shiny)}
             />
           </div>
-          <div className="row-start-3 text-2xl col-start-1 col-end-3 border-2 border-black p-2">
+          <div className="row-start-3 text-2xl col-start-1 col-end-3 sm:border-2 border-black sm:p-2">
             <h2>Evolution Tree</h2>
-            <div className="flex justify-evenly">
+            <div className="flex justify-evenly lg:mt-4 sm:mt-12 mt-4 gap-3 overflow-x-auto">
               {/* evol tree div */}
               {data.evolTree.map((evolution, index) => (
-                <div className="flex flex-col gap-1 justify-center" key={index}>
-                  <img src={evolution.picture} alt="" className="w-18" />
-                  <h2>{evolution.name}</h2>
+                <div className="flex flex-col gap-1 justify-center text-center hover:text-slate-500 cursor-pointer" key={index} onClick={() => displayData(evolution.name)}>
+                  <img src={evolution.picture} alt="" className="h-[100px] w-[100px]" />
+                  <h2 className="sm:text-2xl text-sm">{evolution.name}</h2>
                 </div>
               ))}
             </div>
           </div>
         </div>
-        <div className="grid grid-rows-2 grid-cols-3 gap-3">
-          <div className="border-2 border-black p-2 col-start-1 col-end-4">
+        <div className="grid sm:grid-rows-2 grid-rows-[290px_160px_110px] grid-cols-3 sm:gap-3">
+          <div className="sm:border-2 border-black sm:p-2 col-start-1 col-end-4">
             <h2 className="text-2xl">Abilities</h2>
 
-            <div className="overflow-y-auto h-52">
+            <div className="overflow-y-auto lg:h-52 sm:h-48 h-64 sm:border-0 border-1 rounded-sm sm:p-0 p-2">
               {/* abilities div */}
               {data.abilities.map((ability, index) => (
                 <div key={index}>
-                  <h2 className="text-2xl">{ability.name}</h2>
-                  <h4>{ability.description}</h4>
+                  <h2 className="sm:text-2xl text-lg">{ability.name}</h2>
+                  <h4 className="sm:text-sm text-xs">{ability.description}</h4>
                 </div>
               ))}
             </div>
           </div>
-          <div className="border-2 border-black p-2 col-start-1 col-end-2">
+          <div className="sm:border-2 border-black sm:p-2 col-start-1 sm:col-end-2 col-end-4">
             <h2 className="text-2xl">Moves</h2>{" "}
-            <h4 className="text-md">({data.moves.length})</h4>
-            <div className="overflow-y-auto h-48">
+            <h4 className="sm:text-lg text-xs">({data.moves.length})</h4>
+            <div className="overflow-y-auto lg:h-48 sm:h-44 h-24 sm:border-0 border-1 rounded-sm sm:p-0 p-2">
               {/* moves div */}
               {data.moves.map((move, index) => (
-                <h3 className="text-xl" key={index}>
+                <a href={`https://pokemondb.net/move/${deformatString(move)}`} className="hover:text-slate-500" key={index}>
+                <h3 className="sm:text-xl">
                   {move}
                 </h3>
+                <hr className="lg:hidden block"/>
+                </a>
               ))}
             </div>
           </div>
-          <div className="border-2 border-black p-2 col-start-2 col-end-4">
+          <div className="sm:border-2 border-black sm:p-2 sm:col-start-2 col-start-1 sm:row-start-2 row-start-3 col-end-4">
             <h2 className="text-2xl">Favorites</h2>
-            <div className="overflow-y-scroll h-48 flex flex-col gap-3">
+            <div className="sm:overflow-y-auto overflow-y-visible overflow-x-auto sm:h-48 h-18 flex sm:flex-col flex-row sm:justify-normal justify-center gap-3 sm:border-0 border-1 rounded-sm sm:p-0 p-2">
               {/* favorites div */}
               {fav.map((favorite, index) => (
                 <div
-                  className="flex justify-between place-items-center mx-3"
+                  className="flex sm:justify-between place-items-center sm:gap-0 gap-2 sm:mx-3"
                   key={index}
                 >
                   <div
-                    className="flex gap-2 place-items-center cursor-pointer hover:text-slate-500"
+                    className="flex md:flex-row flex-col lg:gap-2 place-items-center cursor-pointer hover:text-slate-500"
                     onClick={() => displayData(favorite.name.toLowerCase())}
                   >
                     <img
                       src={favorite.image}
                       alt={favorite.name}
-                      className="w-24"
+                      className="w-[60px] h-[60px]"
                     />
-                    <h3 className="text-3xl">{favorite.name}</h3>
+                    <h3 className="text-xl sm:block hidden">{favorite.name}</h3>
                   </div>
                   <button onClick={() => removeFav(favorite.name)}>
-                    <h2 className="text-2xl hover:text-red-500">X</h2>
+                    <h2 className="sm:text-2xl hover:text-red-500">X</h2>
                   </button>
                 </div>
               ))}
             </div>
           </div>
         </div>
+      </div>
+      <div className="flex justify-end">
+      <h4 className="text-xs">created by: Aaron Robinson</h4>
+
       </div>
     </div>
   );
--- src/lib/services.ts
+++ src/lib/services.ts
@@ -11,7 +11,11 @@ function formatString(input: string): string {
     .split("-")
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
     .join(" ");
-};
+}
+
+function deformatString(input:string) {
+  return input.toLowerCase().replace(/\s+/g, '-');
+}
 
 const findPicturebyName = async (pokemon: string) => {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`, {
@@ -42,7 +46,10 @@ const generatePokemonData = async (pokemon: string) => {
     const abilityData = await abilityResponse.json();
     const abilityObj: AbilityTree = {
       name: formatString(abilityData.name),
-      description: abilityData.effect_entries.length != 1 ? abilityData.effect_entries[1].effect : abilityData.effect_entries[0].effect
+      description:
+        abilityData.effect_entries.length != 1
+          ? abilityData.effect_entries[1].effect
+          : abilityData.effect_entries[0].effect,
     };
     abilityArr.push(abilityObj);
   }
@@ -54,10 +61,12 @@ const generatePokemonData = async (pokemon: string) => {
   const speciesData = await speciesResponse.json();
 
   const evolArr: EvolTree[] = [];
+
   if (speciesData.evolves_from_species != null) {
     let response = await fetch(speciesData.evolves_from_species.url);
     let data = await response.json();
 
+    //finding the first evolution
     while (data.evolves_from_species != null) {
       response = await fetch(data.evolves_from_species.url);
       data = await response.json();
@@ -72,17 +81,28 @@ const generatePokemonData = async (pokemon: string) => {
       picture: await findPicturebyName(speciesData.name),
     });
   }
+
   const evolResponse = await fetch(speciesData.evolution_chain.url);
   const evolData = await evolResponse.json();
   let evolChain = evolData.chain;
-  while (evolChain.evolves_to.length != 0) {
-    evolChain = evolChain.evolves_to[0];
-    evolArr.push({
-      name: formatString(evolChain.species.name),
-      picture: await findPicturebyName(evolChain.species.name),
-    });
+  if (evolChain.evolves_to.length > 3) {
+    for (let i = 0; i < evolChain.evolves_to.length; i++) {
+      evolArr.push({
+        name: formatString(evolChain.evolves_to[i].species.name),
+        picture: await findPicturebyName(evolChain.evolves_to[i].species.name),
+      });
+    }
+  } else {
+    while (evolChain.evolves_to.length != 0) {
+      evolChain = evolChain.evolves_to[0];
+      evolArr.push({
+        name: formatString(evolChain.species.name),
+        picture: await findPicturebyName(evolChain.species.name),
+      });
+    }
   }
-//   console.log(evolArr);
+
+  //   console.log(evolArr);
   const pokeData: PokemonData = {
     name: formatString(data.name),
     number: data.id,
@@ -100,15 +120,17 @@ const generatePokemonData = async (pokemon: string) => {
   return pokeData;
 };
 
-const generateFavs = async() => {
-    const favList:IFavorites[] = []
-    const favNames = getLocalStorage()
-    // console.log(favNames)
-    for(let i = 0; i < favNames.length;i++)
-    {
-        favList.push({name:formatString(favNames[i]), image: await findPicturebyName(favNames[i])})
-    }
-    return favList
-}
+const generateFavs = async () => {
+  const favList: IFavorites[] = [];
+  const favNames = getLocalStorage();
+  // console.log(favNames)
+  for (let i = 0; i < favNames.length; i++) {
+    favList.push({
+      name: formatString(favNames[i]),
+      image: await findPicturebyName(favNames[i]),
+    });
+  }
+  return favList;
+};
 
-export { generatePokemonData,generateFavs};
+export { generatePokemonData, generateFavs ,deformatString };
